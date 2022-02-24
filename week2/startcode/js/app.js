2bb527f609c13d2ac8060ced7c22a9c3b0f4859f
Zino Schubert <77681827+1010115@users.noreply.github.com>
2022-02-24 20:39:03 +0100

    Opdracht 2
    speech

--- week2/startcode/js/app.js
+++ week2/startcode/js/app.js
@@ -1,19 +1,37 @@
-let nn
+let classifier;
+let img = document.getElementById('image') ;
+const synth = window.speechSynthesis;
+const btn = document.getElementById('button');
+
+btn.addEventListener("click", say("hi"))
 
 function loadModel() {
-    
+    classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+
 }
 
 function modelLoaded() {
     console.log('Model Loaded!')
 }
 
+
 function classifyImage() {
-    speak(`I think this photo shows a tennis ball!`)
+    classifier.classify(img, (err, results) => { console.log(results); });
+
+     say(`I think this photo shows a tennis ball!`)
+
 }
 
-function speak(message) {
-    
+function say(message) {
+    if (synth.speaking){
+        console.log('speaking')
+        return
+    }
+    if (message !== ' '){
+        let utterThis = new SpeechSynthesisUtterance(message)
+        synth.speak(utterThis);
+    }
 }
 
 loadModel()
+classifyImage()
