9e9febb9e3e0355f9618486a9668b658e269f2cd
Zino Schubert <77681827+1010115@users.noreply.github.com>
2022-02-25 01:06:32 +0100

    phone
    phone

--- week2/startcode/js/app.js
+++ week2/startcode/js/app.js
@@ -1,29 +1,34 @@
 let classifier;
-let img = document.getElementById('image') ;
-let result ;
+let classifiedImg = document.getElementById('image');
+let result;
 const synth = window.speechSynthesis;
-const btn = document.getElementById('button');
-
-const image = document.getElementById('output')
+const classifyBtn = document.getElementById('classify');
+const userImage = document.getElementById('output')
 const fileButton = document.querySelector("#file")
+let desiredResult = {
+    label: "coffee mug",
+    confidenc: 0.5
+}
+let checkBtn = document.getElementById('check');
 
-fileButton.addEventListener("change", (event)=>loadFile(event))
+fileButton.addEventListener("change", (event) => loadFile(event))
+
+classifyBtn.addEventListener("click", () => classifyImage(classifiedImg))
+
+userImage.addEventListener('load', () => userImageUploaded())
+
+checkBtn.addEventListener('click', () => {classifyImage(userImage);compare(result)})
 
 function loadFile(event) {
-	image.src = URL.createObjectURL(event.target.files[0])
+    userImage.src = URL.createObjectURL(event.target.files[0])
 }
-
-
-btn.addEventListener("click", () => classifyImage())
 
 function loadModel() {
     classifier = ml5.imageClassifier('MobileNet', modelLoaded);
 
 }
 
-image.addEventListener('load', () => userImageUploaded())
-
-function userImageUploaded(){
+function userImageUploaded() {
     console.log("The image is now visible in the DOM")
 }
 
@@ -33,26 +38,35 @@ function modelLoaded() {
 }
 
 
-function classifyImage() {
-    classifier.classify(img, (err, results) => { (result = results[0].label); });
-    
-     say(`I think this photo shows a ${result}!`)
+function classifyImage(img) {
+    classifier.classify(img, (err, results) => { (result = results[0]); });
 
+    if (result !== undefined) {
+        console.log(`I think this photo shows a ${result.label}, I am ${Math.round(result.confidence * 100)} % sure !`);
+        say(`I think this photo shows a ${result.label}!, I am ${Math.round(result.confidence * 100)} % sure !`)
+    }
 }
 
 function say(message) {
-    console.log("say function triggered");
-    if (synth.speaking){
+    if (synth.speaking) {
         console.log('speaking')
         return
     }
-    if (message !== ' '){
+    if (message !== ' ') {
         let utterThis = new SpeechSynthesisUtterance(message)
         synth.speak(utterThis);
     }
 }
 
-
+function compare(result) {
+    if ( result.label !== undefined && result.label == desiredResult.label) {
+        say(`Congratulations this is a ${result.label}`);
+        console.log(`Congratulations this is a ${result.label}`)
+    } else {
+        say(`this doesn't look like a ${desiredResult.label}, this looks more like a ${result.label}`);
+        console.log(`this doesn't look like a ${desiredResult.label}, this looks more like a ${result.label}`)
+    }
+}
 
 loadModel()
-classifyImage()
+
