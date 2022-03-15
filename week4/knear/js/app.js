5062f4f4a2212051409d90f63f6de403e94f27dd
Zino Schubert <77681827+1010115@users.noreply.github.com>
2022-03-15 14:38:19 +0100

    hw?
    hwhwhwhwhwhwhwhwhw

--- week4/knear/js/app.js
+++ week4/knear/js/app.js
@@ -1,9 +1,21 @@
 const k = 3
 const machine = new kNear(k)
 
+
 // training - todo: meerdere voorbeelden voor cats en dogs nodig!
 machine.learn([11, 5, 10], 'cat')
 
+machine.learn([18, 9.2, 8.1], 'cat')
+machine.learn([17,9.1,9], 'cat')
+machine.learn([16,9.0,10], 'cat')
+
+machine.learn([20.1, 17, 15.5], 'dog')
+machine.learn([23.5, 20, 20],'dog')
+machine.learn([21, 16.7, 16],'dog')
+
 // predicting
-let prediction = machine.classify([7, 7, 7])
+let prediction = machine.classify([20, 9, 7])
 console.log(`I think it's a ${prediction}`)
+let h1= document.createElement('h1');
+h1.innerText = prediction;
+document.body.appendChild(h1);
