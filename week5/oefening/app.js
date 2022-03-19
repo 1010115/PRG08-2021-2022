560a42906f09c29d92655a26f68a3a7d2416e39e
Zino Schubert <77681827+1010115@users.noreply.github.com>
2022-03-19 17:37:52 +0100

    hw 5
    hw 5

--- week5/oefening/app.js
+++ week5/oefening/app.js
@@ -1,17 +1,18 @@
 import { DecisionTree } from "./libraries/decisiontree.js"
 import { VegaTree } from "./libraries/vegatree.js"
 
-const csvFile = "./data/simpsons.csv"
-const trainingLabel = "Gender"
-const ignoredColumns = ['Name']
+const csvFile = "./data/mushrooms.csv"
+const trainingLabel = "class"
+const ignoredColumns = []
+let guess = 0
 
 // inladen csv data
-function loadData() {
+function loadData() {j
     Papa.parse(csvFile, {
         download: true,
         header: true,
         dynamicTyping: true,
-        complete: (results) => console.log(results.data) // train het model met deze data
+        complete: (results) => trainModel(results.data) // train het model met deze data
     })
 }
 
@@ -19,14 +20,42 @@ function loadData() {
 // MACHINE LEARNING - Bouw de Decision Tree
 //
 function trainModel(data) {
+    data.sort( () => (Math.random() - 0.5))
+    let trainData = data.slice(0, Math.floor(data.length * 0.8))
+    let testData = data.slice(Math.floor(data.length * 0.8) + 1)
     let decisionTree = new DecisionTree({
         ignoredAttributes: ignoredColumns,
-        trainingSet: data,
+        trainingSet: trainData,
         categoryAttr: trainingLabel
     })
 
+    let shroom = testData[0]
+    let shroomPrediction = decisionTree.predict(shroom)
+    console.log(`Is poisonous : ${shroomPrediction}`)
+
+
+    let json = decisionTree.toJSON()
     // Teken de boomstructuur - DOM element, breedte, hoogte, decision tree
-    let visual = new VegaTree('#view', 800, 400, decisionTree.toJSON())
+    let visual = new VegaTree('#view', 800, 400, json)
+    for (let i = 0; i < testData.length; i++){
+        testShroom(testData[i], decisionTree, shroom)
+       
+    }
+
+    console.log(guess / testData.length * 100)
+
+}
+
+function testShroom(shroomie, decisionTree, shroom) {
+    const shroomWithoutLabel = Object.assign({}, shroomie)
+    delete shroomWithoutLabel.class
+
+    let prediction = decisionTree.predict(shroomWithoutLabel)
+
+    if (prediction == shroom.class) {
+        guess++
+        console.log("Goed")
+    }
 }
 
 loadData()
