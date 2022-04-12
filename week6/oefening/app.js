f557a027a2691eb88a748078773b0f85a7e326e4
Zino Schubert <77681827+1010115@users.noreply.github.com>
2022-04-12 23:26:08 +0200

    hw

--- week6/oefening/app.js
+++ week6/oefening/app.js
@@ -1,8 +1,17 @@
 import { VegaScatterplot } from "./libraries/vegascatterplot.js"
 import { createFakedata } from "./libraries/fakedata.js"
 
-let nn
+let options = {task: 'regression', debug: true}
+let nn = ml5.neuralNetwork(options)
 let fakeData = createFakedata()
+fakeData.sort(()=> (Math.random( - 0.5)))
+for (let car of fakeData){
+    nn.addData({ horsepower: car.horsepower}, { mpg: car.mpg})
+} 
+nn.normalizeData()
+nn.train({ epochs: 10}, () => trainingFinished())
+
+
 let plot
 
 //
@@ -10,8 +19,7 @@ let plot
 //
 async function drawScatterPlot() {
     plot = new VegaScatterplot()
-    // await plot.initialise("horsepower", "mpg", 600, 400, fakeData)
-    
+    await plot.initialise("horsepower", "mpg", 600, 400, fakeData)
 }
 
 //
@@ -35,22 +43,33 @@ async function createNeuralNetwork() {
 // predictions
 //
 async function trainingFinished() {
+    console.log(" finished training")
     // doe een enkele voorspelling om te zien of alles werkt
     let testCar = { horsepower: 220 }
 
+    let results = await nn.predict(testCar)
+    console.log(results)
 
 
+    let prediction = results[0].value
+    console.log(`deze auto zal een verbruik hebben van: ${prediction}`)
     // maak een voorspelling voor elk punt op de X as
     let predictions = []
+
     for(let i = 0; i< 400; i++) {
-        // let prediction = ....
-        // predictions.push(...)
+        let results = await nn.predict({ horsepower: i})
+        let prediction = results[0].value
+        
+        predictions.push({horsepower: i, mpg: prediction})
+
     }
+    plot.addPoints(predictions)
+    console.log(predictions)
 
 
     // stuur nu de hele predictions array naar de scatterplot met "plot.addPoints"
     // ...
 }
+drawScatterPlot()
 
 // start de applicatie
-// drawScatterPlot()
